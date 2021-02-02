4367fd719d66d7de36548ab44cbe706f6d7cf5af
Aurimas Blažulionis <0x60@pm.me>
2021-02-02 12:55:41 +0000

    Correct a typo

--- ksyms.h
+++ ksyms.h
@@ -3,7 +3,7 @@
 
 #define KSYMDEC(x) extern typeof(&x) _##x;
 #define KSYMDEF(x) void *_##x = NULL;
-#define KSYM(x) ((typeof(&x)_##x)
+#define KSYM(x) ((typeof(&x))_##x)
 #define KSYMINIT(x) (_##x = (void *)kallsyms_lookup_name(#x))
 #define KSYMINIT_FAULT(x) if (!KSYMINIT(x)) return EBADF
 
